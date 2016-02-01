82ae8034708b57ade2b8dd9d65b287da7adada5d
ItsTristan <tmeleshk@ualberta.ca>
2016-02-01 12:37:33 -0700

    Initial Commit

---
+++ src/ca/ualberta/Kinematics.java
@@ -0,0 +1,64 @@
+package ca.ualberta;
+
+import java.awt.Point;
+import java.io.File;
+
+public class Kinematics {
+	
+	public static final int[] joint_lengths = {10, 10};	// the length of each joint in mm.
+	
+	/**
+	 * Given a set of theta values for the joint angles, returns
+	 * the (x,y)-point location of the end effector
+	 * @param theta
+	 * @return
+	 */
+	public static Point forwardKinematics(int theta[]) {
+		return null;
+	}
+	
+	/**
+	 * Given a point, computes and returns the joint angles
+	 * required to achieve the target
+	 * @param target
+	 * @return final theta values
+	 */
+	public static int[] inverseKinematics(Point target) {
+		return null;
+	}
+	
+	/**
+	 * Given two points, returns a list of target points for the robot
+	 * to move towards along a line, or null if the target has been reached.
+	 * @param start
+	 * @param end
+	 * @param resolution the number of intermediate points to generate 
+	 * @return
+	 */
+	public static Point[] createLinePath(Point current, Point start, Point end, int resolution) {
+		return null;
+	}
+	
+	/**
+	 * Given n points, returns a list of target points for the robot to move
+	 * towards along an arc, or null if the target has been reached.
+	 * @param current
+	 * @param start
+	 * @param mid
+	 * @param end
+	 * @param resolution
+	 * @return
+	 */
+	public static Point[] createArcPath(Point current, Point points[], int resolution) {
+		return null;
+	}
+	
+	/**
+	 * Given a 2D image, returns a path that traces the image.
+	 * @param current
+	 * @return
+	 */
+	public static Point[] createPicturePath(Point current, File image) {
+		return null;
+	}
+}
